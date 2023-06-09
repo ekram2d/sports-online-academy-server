b7c9b0c752691d6dec19f80e28771ae3a24c1557
Ekram Hossain <ekramhossain117@gmail.com>
2023-06-09 18:15:04 +0600

    make admin or instructor part done

--- index.js
+++ index.js
@@ -78,12 +78,17 @@ async function run() {
 
 
     // users collection 
+   
+    app.get('/users',async(req,res)=>{
 
+       const result = await userssdatabase.find().toArray();
+       res.send(result);
+    })
    app.post('/users',async(req,res)=>{
     const user = req.body;
     const query = {email:user.email}
     const existingUser = await userssdatabase.findOne(query);
-    console.log('exit',existingUser);
+    // console.log('exit',existingUser);
     if(existingUser) {
       return res.send({message:'user already exists'})
     }
@@ -91,7 +96,22 @@ async function run() {
     const result = await userssdatabase.insertOne(user);
     res.send(result);
    })
+   
 
+   app.patch('/users/admin/:id',async(req,res)=>{
+    
+    const id=req.params.id;
+    const filter = {_id: new ObjectId(id)};
+    const updateDoc = {
+      $set: {
+        role: req.body.field1
+      },
+    };
+// console.log(req.body.field1);
+    const result =await userssdatabase.updateOne(filter,updateDoc);
+    res.send(result);
+
+   })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
